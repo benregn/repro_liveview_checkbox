95a662b3233346faa23e027f24313bb44c986773
Tomas Thor Jonsson <benregn@gmail.com>
2022-11-04 13:41:09 +0100

    Generate users

--- lib/my_app_web/router.ex
+++ lib/my_app_web/router.ex
@@ -18,10 +18,12 @@ defmodule MyAppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-  end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", MyAppWeb do
-  #   pipe_through :api
-  # end
+    live "/users", UserLive.Index, :index
+    live "/users/new", UserLive.Index, :new
+    live "/users/:id/edit", UserLive.Index, :edit
+
+    live "/users/:id", UserLive.Show, :show
+    live "/users/:id/show/edit", UserLive.Show, :edit
+  end
 end
